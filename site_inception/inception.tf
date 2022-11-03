92ec939d82c6a822489db3ef786307dabc726e25
Pedro Pessoa <prp@pobox.com>
2022-11-03 16:10:38 +0000

    initial site_inception

---
+++ site_inception/inception.tf
@@ -0,0 +1,29 @@
+module "backends" {
+
+  source = "bitbucket.org/asolidodev/my_inception.git?ref=use-official-modules"
+
+  for_each = var.include_inception_project ? toset(concat(var.inception_projects, ["inception"])) : toset(var.inception_projects)
+
+  my_inception_organization          = var.organization
+  my_inception_environment           = var.environment
+  my_inception_domain                = "site"
+  my_inception_project               = each.key
+  my_inception_enable_versioning     = var.enable_versioning
+  my_inception_disable_force_destroy = var.disable_force_destroy
+}
+
+resource "local_file" "backend_configs" {
+
+  for_each = var.include_inception_project ? toset(concat(var.inception_projects, ["inception"])) : toset(var.inception_projects)
+
+  content = templatefile("backend.tpl", {
+    organization = var.organization,
+    environment  = var.environment,
+    site         = each.key,
+    profile      = var.provider_profile,
+    region       = var.provider_region
+    }
+  )
+  filename        = "../configs/backends/${var.organization}-${var.environment}-${each.key}.tfvars"
+  file_permission = "0664"
+}
---
+++ site_inception/terraform.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_version = ">= 1"
+
+  # comment the following when initially preparing the very first envs
+  #backend "s3" {}
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.13.0"
+    }
+  }
+}
