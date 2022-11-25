b461b2ed6fb3256ab7fbdc44980bcbbefbd19904
Lucas Cañero <54242962+lucascanero@users.noreply.github.com>
2022-11-25 17:33:31 +0000

    Update config_template.sh

--- config_template.sh
+++ config_template.sh
@@ -2,46 +2,46 @@
 #USAGE:
 #   ./config_template [FLAG] [STRING]
 #FLAGS:
-#    -p <project_name>        The name for your project
+#    -o <organization_name>   The name for your organization
 #
-#    -d <domain>              The domain of your project
+#    -d <domain>              The domain of your organization
 
-#    -e <environment_name>    The environment name of your project
+#    -e <environment_name>    The environment name of your organization
 #
 while getopts d:e:p: flag
 do
     case "${flag}" in
         d) domain=${OPTARG};;
         e) env=${OPTARG};;
-        p) project=${OPTARG};;
-        *) echo "usage: $0 [-d <domain>] [-e <environment>] [ -p <project>]" >&2
+        o) organization=${OPTARG};;
+        *) echo "usage: $0 [-d <domain>] [-e <environment>] [ -p <organization>]" >&2
     esac
 done
 # Rename configs/backends/*.tfvars
-mv configs/backends/project-env-inception.tfvars configs/backends/"$project"-"$env"-inception.tfvars 
+mv configs/backends/organization-env-inception.tfvars configs/backends/"$organization"-"$env"-inception.tfvars 
 # Rename configs/variables/*.tfvars
-mv configs/variables/project-env-inception.tfvars configs/variables/"$project"-"$env"-inception.tfvars 
-mv configs/variables/project-env.tfvars configs/variables/"$project"-"$env".tfvars 
+mv configs/variables/organization-env-inception.tfvars configs/variables/"$organization"-"$env"-inception.tfvars 
+mv configs/variables/organization-env.tfvars configs/variables/"$organization"-"$env".tfvars 
 
 # Replace strings with the variables
-sed -i "s/project_var/$project/" configs/backends/"$project"-"$env"-inception.tfvars 
+sed -i "s/organization_var/$organization/" configs/backends/"$organization"-"$env"-inception.tfvars 
 
-sed -i "s/project_var/$project/" configs/variables/"$project"-"$env"-inception.tfvars 
-sed -i "s/project_var/$project/" configs/variables/"$project"-"$env".tfvars 
-sed -i "s/project_var/$project/" configs/variables/global.tfvars 
+sed -i "s/organization_var/$organization/" configs/variables/"$organization"-"$env"-inception.tfvars 
+sed -i "s/organization_var/$organization/" configs/variables/"$organization"-"$env".tfvars 
+sed -i "s/organization_var/$organization/" configs/variables/global.tfvars 
 
-sed -i "s/project_var/$project/" configs/settings.yml
+sed -i "s/organization_var/$organization/" configs/settings.yml
 
 
-sed -i "s/env_var/$env/" configs/backends/"$project"-"$env"-inception.tfvars 
+sed -i "s/env_var/$env/" configs/backends/"$organization"-"$env"-inception.tfvars 
 
-sed -i "s/env_var/$env/" configs/variables/"$project"-"$env"-inception.tfvars 
-sed -i "s/env_var/$env/" configs/variables/"$project"-"$env".tfvars 
+sed -i "s/env_var/$env/" configs/variables/"$organization"-"$env"-inception.tfvars 
+sed -i "s/env_var/$env/" configs/variables/"$organization"-"$env".tfvars 
 
 sed -i "s/env_var/$env/" configs/settings.yml
 
 
-sed -i "s/domain_var/$domain/" configs/variables/"$project"-"$env".tfvars 
+sed -i "s/domain_var/$domain/" configs/variables/"$organization"-"$env".tfvars 
 
 sed -i "s/domain_var/$domain/" configs/settings.yml
 
