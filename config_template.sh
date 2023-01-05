d226f989231baee5b59bb6d6e56ac0ece5bdf7f9
Tiago Carrondo <tcarrondo@angulosolido.pt>
2023-01-05 12:41:13 +0000

    andd region var and KISS seds

--- config_template.sh
+++ config_template.sh
@@ -8,42 +8,26 @@
 
 #    -e <environment_name>    The environment name of your organization
 #
-while getopts d:e:o: flag
-do
+while getopts d:e:o:r: flag; do
     case "${flag}" in
-        d) domain=${OPTARG};;
-        e) env=${OPTARG};;
-        o) organization=${OPTARG};;
-        *) echo "usage: $0 [-d <domain>] [-e <environment>] [ -o <organization>]" >&2
+    d) domain=${OPTARG} ;;
+    e) env=${OPTARG} ;;
+    o) organization=${OPTARG} ;;
+    r) region=${OPTARG} ;;
+    *) echo "usage: $0 [-d <domain>] [-e <environment>] [ -o <organization>] [ -r <region>]" >&2 ;;
     esac
 done
 # Rename configs/backends/*.tfvars
-mv configs/backends/organization-env-inception.tfvars configs/backends/"$organization"-"$env"-inception.tfvars 
+mv configs/backends/organization-env-inception.tfvars configs/backends/"$organization"-"$env"-inception.tfvars
 # Rename configs/variables/*.tfvars
-mv configs/variables/organization-env-inception.tfvars configs/variables/"$organization"-"$env"-inception.tfvars 
-mv configs/variables/organization-env.tfvars configs/variables/"$organization"-"$env".tfvars 
+mv configs/variables/organization-env-inception.tfvars configs/variables/"$organization"-"$env"-inception.tfvars
+mv configs/variables/organization-env.tfvars configs/variables/"$organization"-"$env".tfvars
 
 # Replace strings with the variables
-sed -i "s/organization_var/$organization/" configs/backends/"$organization"-"$env"-inception.tfvars 
-
-sed -i "s/organization_var/$organization/" configs/variables/"$organization"-"$env"-inception.tfvars 
-sed -i "s/organization_var/$organization/" configs/variables/"$organization"-"$env".tfvars 
-sed -i "s/organization_var/$organization/" configs/variables/global.tfvars 
-
-sed -i "s/organization_var/$organization/" configs/settings.yml
-
-
-sed -i "s/env_var/$env/" configs/backends/"$organization"-"$env"-inception.tfvars 
-
-sed -i "s/env_var/$env/" configs/variables/"$organization"-"$env"-inception.tfvars 
-sed -i "s/env_var/$env/" configs/variables/"$organization"-"$env".tfvars 
-
-sed -i "s/env_var/$env/" configs/settings.yml
-
-
-sed -i "s/domain_var/$domain/" configs/variables/"$organization"-"$env".tfvars 
-
-sed -i "s/domain_var/$domain/" configs/settings.yml
+find configs/ -type f -exec sed -i "s/region_var/$region/" {} +
+find configs/ -type f -exec sed -i "s/domain_var/$domain/" {} +
+find configs/ -type f -exec sed -i "s/env_var/$env/" {} +
+find configs/ -type f -exec sed -i "s/organization_var/$organization/" {} +
 
 echo "Template Configuration replaced!"
-exit 
+exit
